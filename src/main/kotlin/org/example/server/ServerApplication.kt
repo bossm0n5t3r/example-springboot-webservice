2d97ecd04af92b3863d875e253d240f49a45b599
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-08-07 21:21:03 +0900

    Add HelloController and HelloControllerTest

---
+++ src/main/kotlin/org/example/server/ServerApplication.kt
@@ -0,0 +1,12 @@
+package org.example.server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ServerApplication::class.java, *args)
+}
