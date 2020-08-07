ab5523a36785b2e09883a9ac103a94c55ebb0169
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-08-07 21:20:13 +0900

    Add testApi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 val kotlinVersion: String by project
 val springBootVersion: String by project
+val mockitoVersion: String by project
+val junitJupiterVersion: String by project
 
 buildscript {
     val kotlinVersion: String by project
@@ -23,6 +25,7 @@ plugins {
 
     kotlin("jvm") version kotlinVersion
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    id("idea")
 }
 
 repositories {
@@ -33,8 +36,50 @@ repositories {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+apply(plugin = "org.jetbrains.kotlin.jvm")
+apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+apply(plugin = "java")
+apply(plugin = "idea")
+apply(plugin = "eclipse")
+apply(plugin = "org.springframework.boot")
+apply(plugin = "io.spring.dependency-management")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_14
+    targetCompatibility = JavaVersion.VERSION_14
+}
 
 dependencies {
+    api("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     api("org.springframework.boot:spring-boot-starter-web")
-    api("org.springframework.boot:spring-boot-starter-test")
+
+    testApi("org.springframework.boot:spring-boot-starter-test")
+    testApi("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testApi("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+    testApi("org.mockito:mockito-core:$mockitoVersion")
+    testApi("org.mockito:mockito-junit-jupiter:$mockitoVersion")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+}
+
+tasks.compileKotlin {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xuse-experimental=kotlin.Experimental")
+        jvmTarget = "13"
+    }
+}
+
+tasks.compileTestKotlin {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xuse-experimental=kotlin.Experimental")
+        jvmTarget = "13"
+    }
+}
+
+tasks.test {
+    maxHeapSize = "1536m"
+    useJUnitPlatform()
 }
