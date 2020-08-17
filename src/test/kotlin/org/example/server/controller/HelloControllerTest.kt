8df386b410988ec718a31469ec70293ae15d1eb1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-08-17 16:07:26 +0900

    Edit HelloControllerTest

--- src/test/kotlin/org/example/server/controller/HelloControllerTest.kt
+++ src/test/kotlin/org/example/server/controller/HelloControllerTest.kt
@@ -2,27 +2,26 @@ package org.example.server.controller
 
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@RunWith(SpringRunner::class)
 @WebMvcTest
-class HelloControllerTest(
+class HelloControllerTest {
     @Autowired
-    private val mvc: MockMvc
-) {
+    lateinit var mvc: MockMvc
+
     @Test
     fun `hello가 리턴된다`() {
         val hello = "hello"
 
-        mvc.perform(get("/hello"))
+        mvc.perform(
+            get("/hello")
+        )
             .andExpect(status().isOk)
             .andExpect(content().string(hello))
     }
