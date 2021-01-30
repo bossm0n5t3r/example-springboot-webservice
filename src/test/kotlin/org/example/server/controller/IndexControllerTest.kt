ef3a9eed24627dafbd72512879d481d4e0c2ee37
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-01-31 06:53:04 +0900

    Disabled Test for memory

--- src/test/kotlin/org/example/server/controller/IndexControllerTest.kt
+++ src/test/kotlin/org/example/server/controller/IndexControllerTest.kt
@@ -1,11 +1,13 @@
 package org.example.server.controller
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 
+@Disabled
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IndexControllerTest {
     @Autowired
